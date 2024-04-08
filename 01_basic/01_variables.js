2be39876e43b07668d94099164862516387259cf
ashutosh-kumar-vmcc <166106854+ashutosh-kumar-vmcc@users.noreply.github.com>
2024-04-08 12:32:52 +0000

    var

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 142553
+let accountEmail="ashu@google.com"
+var accountPassword = "98765"
+ 
+accountCity="Jaipur"
+
+//accountId=583863
+
+accountEmail="hc@hc.com"
+accountPassword="53546347"
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
